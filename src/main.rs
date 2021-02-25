1d723a09cbdb1ff63fa727d65486ec1a664222e5
frontierpsycho <nonsenseisay@protonmail.com>
2021-02-25 10:41:43 +0100

    Fully print cards.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ mod set;
 use set::{CardNumber::*, CardShading::*, CardColour::*, CardShape::*};
 
 fn main() {
-    println!("Hello, world!");
-
-    let card = set::Card { number: One , shading: Solid, colour: Purple, shape: Diamond };
+    let card = set::Card { number: Two , shading: Solid, colour: Purple, shape: Diamond };
 
     println!("Card: {}", card);
 }
--- src/set.rs
+++ src/set.rs
@@ -1,5 +1,6 @@
 use std::fmt::{self, Formatter, Display};
 
+#[derive(PartialEq, Eq)]
 pub enum CardNumber {
     One,
     Two,
@@ -23,16 +24,49 @@ pub enum CardShading {
     Outlined,
 }
 
+// TODO there must be an easier way
+impl fmt::Display for CardShading {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match *self {
+           CardShading::Solid => write!(f, "solid"),
+           CardShading::Striped => write!(f, "striped"),
+           CardShading::Outlined => write!(f, "outlined"),
+       }
+    }
+}
+
 pub enum CardColour {
     Red,
     Purple,
     Green,
 }
 
+// TODO there must be an easier way
+impl fmt::Display for CardColour {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match *self {
+           CardColour::Red => write!(f, "red"),
+           CardColour::Purple => write!(f, "purple"),
+           CardColour::Green => write!(f, "green"),
+       }
+    }
+}
+
 pub enum CardShape {
     Oval,
     Diamond,
     Squiggle,
+}
+
+// TODO there must be an easier way
+impl fmt::Display for CardShape {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match *self {
+           CardShape::Oval => write!(f, "oval"),
+           CardShape::Diamond => write!(f, "diamond"),
+           CardShape::Squiggle => write!(f, "squiggle"),
+       }
+    }
 }
 
 // TODO don't use pub fields, make a constructor
@@ -46,7 +80,11 @@ pub struct Card {
 impl Display for Card {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         // TODO why should this result be used? Error checking?
-        write!(f, "{}", self.number);
+        write!(f, "{} {} {} {}", self.number, self.shading, self.colour, self.shape);
+
+        if self.number != CardNumber::One {
+            write!(f, "s");
+        }
 
         Ok(())
     }
