ef3cae077a4da4b0f616526a20e7932c4d11d7e2
frontierpsycho <nonsenseisay@protonmail.com>
2021-02-26 11:45:26 +0100

    Added solving module, implemented attribute tests and unit tests.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use set::card::Card;
 // use set::{CardNumber::*, CardShading::*, CardColour::*, CardShape::*};
+//
+// TODO
+// - write unit tests for solving
 
 fn main() {
     println!("Card: {}", Card::new());
